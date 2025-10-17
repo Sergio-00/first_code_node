f905e5a407a4df3fad828a1ee13996d4962b63ea
Sergio-00 <ser.and60@gmail.com>
2025-10-17 06:54:21 -0500

    Multiplos de 3

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello World.")
+
+// Crear un programa que identifique
+// cuantos multiplos de 3 hay en un 
+// rango del 0 al 100 y que los imprima
+// en consola
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log(i)
+  }
+}
